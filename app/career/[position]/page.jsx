9818d826e8b0e514f319edeb28700040be9c927a
Jaspreet <jaspreetsingh44555@gmail.com>
2024-09-16 17:50:40 +0530

    feat: Add lazy loading for images on career page

--- app/career/[position]/page.jsx
+++ app/career/[position]/page.jsx
@@ -33,6 +33,7 @@ const videos = [
 
 const JobDescription = ({ params }) => {
   const { position } = params;
+  const [activeTab, setActiveTab] = useState("basic");
 
   const positionsData = {
     "full-stack-engineer": {
@@ -415,8 +416,6 @@ const JobDescription = ({ params }) => {
     "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
-
-  const [activeTab, setActiveTab] = useState("basic");
 
   return (
     <>
