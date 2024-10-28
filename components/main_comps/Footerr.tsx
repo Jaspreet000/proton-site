0eeb9ee42eecd5851060aac0cb2f399e30354128
Jaspreet <jaspreetsingh44555@gmail.com>
2024-10-28 16:18:10 +0530

    Optimize lazy loading for images in AdvisorSection component

--- components/main_comps/Footerr.tsx
+++ components/main_comps/Footerr.tsx
@@ -2,16 +2,8 @@
 "use client";
 
 import React from "react";
-import {
-  FaFlagUsa,
-  FaMapMarkerAlt,
-  FaLeaf,
-  FaFlag,
-  FaMapSigns,
-} from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-// import Link from 'next/link';
-import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
@@ -23,138 +15,104 @@ import Image from "next/image";
 
 export default function Footer() {
   const router = useRouter();
-  const handleButtonClick = () => {
-    router.push("/Contact");
-  };
+
   return (
-    <footer className="bg-white text-gray-700 py-10 mt-9">
-      <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-wrap justify-between items-center">
-          {/* Logo and Social Media */}
-          <div className="w-full sm:w-1/2 lg:w-1/4 mb-8 pr-2 lg:ml-[-50px]">
-            {/* <h2 className="text-2xl font-bold mb-2">Proton Datalabs</h2> */}
-            <div className="flex justify-center">
+    <footer className="bg-white text-gray-700 py-10">
+      {/* Outer Container with Padding */}
+      <div className="px-6 md:px-28 lg:px-12">
+        <div className="max-w-7xl mx-auto">
+          {/* Container for all footer sections */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+            {/* Logo and Copyright */}
+            <div className="flex flex-col items-center sm:items-start md:items-start lg:items-start">
               <Image
                 src="/assets/images/logo.png"
                 alt="Proton Datalabs"
-                width={170}
+                width={150}
                 height={50}
+                className="mb-4"
               />
-            </div>
-            <p className="text-sm text-center text-gray-500 mb-4">
-              Copyright © 2021 Proton Datalabs
-              <br />
-              All rights reserved
-            </p>
-          </div>
-
-          {/* Company Links */}
-          <div className="w-full sm:w-1/2 lg:w-1/4 mb-8">
-            <h3 className="font-bold text-lg mb-4">Explore</h3>
-            <ul>
-              <li>
-                <Link href="#" legacyBehavior passHref>
-                  <span className="text-gray-500 hover:text-gray-700 block mb-2">
-                    Home
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/#services" legacyBehavior passHref>
-                  <span className="text-gray-500 hover:text-gray-700 block mb-2">
-                    Services
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/blogs" legacyBehavior passHref>
-                  <span className="text-gray-500 hover:text-gray-700 block mb-2">
-                    Blogs
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/career" legacyBehavior passHref>
-                  <span className="text-gray-500 hover:text-gray-700 block mb-2">
-                    Career
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/Contact" legacyBehavior passHref>
-                  <span className="text-gray-500 hover:text-gray-700 block mb-2">
-                    Contact Us
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Get Updates */}
-          <div className="w-full sm:w-1/2 lg:w-1/4 mb-8 lg:ml-[-40px]">
-            <h3 className="font-bold text-lg mb-4">Location</h3>
-            <ul>
-              <li className="flex items-center">
-                <US />
-                <span className="ml-2">Seattle, USA</span>
-              </li>
-              <li className="flex items-center mt-2">
-                <Canada />
-                <span className="ml-2">Toronto, Canada</span>
-              </li>
-              <li className="flex items-center mt-2">
-                <Netherland />
-                <span className="ml-2">Amsterdam, Netherlands</span>
-              </li>
-              <li className="flex items-center mt-2">
-                <India />
-                <span className="ml-2">Bangalore, India</span>
-              </li>
-            </ul>
-          </div>
-
-          {/* Support Links */}
-          <div className="w-full sm:w-1/2 lg:w-1/4 mb-8 mt-[-40px]">
-            <div className="flex flex-col h-[130px] pt-[30px]">
-              <p className="text-[14px] mt-5">
-                You can also reach us by clicking the button below
+              <p className="text-sm text-gray-500 text-center">
+                © 2021 Proton Datalabs
+                <br />
+                All rights reserved
               </p>
+            </div>
+
+            {/* Explore Links */}
+            <div className="flex flex-col items-start">
+              <h3 className="font-bold text-lg mb-4">Explore</h3>
+              <ul className="space-y-2">
+                {["Home", "Services", "Blogs", "Career", "Contact Us"].map((item, index) => (
+                  <li key={index}>
+                    <Link href={`/${item.toLowerCase().replace(" ", "-")}`}>
+                      <span className="text-gray-500 hover:text-gray-700">
+                        {item}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Locations */}
+            <div className="flex flex-col items-start">
+              <h3 className="font-bold text-lg mb-4">Location</h3>
+              <ul className="space-y-2">
+                {[{ component: <US />, label: "Seattle, USA" },
+                  { component: <Canada />, label: "Toronto, Canada" },
+                  { component: <Netherland />, label: "Amsterdam, Netherlands" },
+                  { component: <India />, label: "Bangalore, India" }
+                ].map((location, index) => (
+                  <li key={index} className="flex items-center">
+                    {location.component}
+                    <span className="ml-2">{location.label}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Connect and Follow Us */}
+            <div className="flex flex-col items-start">
+              <h3 className="font-bold text-lg mb-4">Connect With Us</h3>
               <button
-                className="bg-blue-950 dark:bg-white rounded-[12px] w-fit h-[34px] text-[12px] text-white dark:text-black px-4 py-2 mt-[12px]"
-                onClick={handleButtonClick}
+                onClick={() => router.push("/Contact")}
+                className="bg-blue-950 text-white rounded-md px-6 py-2 text-sm hover:bg-blue-900 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 mb-6"
               >
                 Contact Us
               </button>
-            </div>
-
-            <div className="flex space-x-4 mt-10">
-              <a href="#" className="text-gray-500 hover:text-gray-700">
-                <FaInstagram className="h-6 w-6" />
-              </a>
-              <Link
-                href="https://www.linkedin.com/in/protondatalabs/"
-                className="text-gray-500 hover:text-gray-700"
-              >
-                <FaLinkedin className="h-6 w-6" />
-              </Link>
-              <Link
-                href="https://x.com/Proton_Datalabs"
-                className="text-gray-500 hover:text-gray-700"
-              >
-                <FaXTwitter className="h-6 w-6" />
-              </Link>
-              <a href="#" className="text-gray-500 hover:text-gray-700">
-                <FaYoutube className="h-6 w-6" />
-              </a>
+              <h3 className="font-bold text-lg mb-4">Follow Us</h3>
+              <div className="flex space-x-4">
+                {[
+                  { href: "#", icon: <FaInstagram className="h-6 w-6" />, label: "Instagram" },
+                  { href: "https://www.linkedin.com/in/protondatalabs/", icon: <FaLinkedin className="h-6 w-6" />, label: "LinkedIn" },
+                  { href: "https://x.com/Proton_Datalabs", icon: <FaXTwitter className="h-6 w-6" />, label: "Twitter" },
+                  { href: "#", icon: <FaYoutube className="h-6 w-6" />, label: "YouTube" }
+                ].map((social, index) => (
+                  <a key={index} href={social.href} className="text-gray-500 hover:text-gray-700 transition-colors rounded-full p-2">
+                    {social.icon}
+                    <span className="sr-only">{social.label}</span>
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        {/* Horizontal Line */}
-        <hr className="border-t-2 border-gray-300 mt-10" />
 
-        {/* Policy Links */}
+          {/* Horizontal Line */}
+          <hr className="border-t-2 border-gray-300 mt-10" />
+
+          {/* Policy Links
+          <div className="flex flex-wrap justify-center items-center mt-6 text-sm text-gray-500 gap-4">
+            {["Terms of Service", "Acceptable Use Policy", "Privacy Statement", "Cookie Preferences"].map((policy, index) => (
+              <Link key={index} href={`/${policy.toLowerCase().replace(/ /g, "-")}`}>
+                {policy}
+              </Link>
+            ))}
+          </div> */}
+
+          {/* Policy Links */}
         <div className="flex flex-wrap justify-center items-center mt-6 text-sm text-gray-500">
-          <div className="flex flex-wrap justify-center gap-4 w-full sm:w-[90%] text-center">
+          <div className="flex flex-wrap justify-center gap-5 w-full sm:w-[90%] text-center">
             <span>
               <Link href="/terms-and-conditions">Terms of Service</Link>
             </span>
@@ -168,6 +126,7 @@ export default function Footer() {
               <Link href="/cookies">Cookie Preferences</Link>
             </span>
           </div>
+        </div>
         </div>
       </div>
     </footer>
