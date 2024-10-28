76c7f4f3e760a4ade896738dfdd8894002fd4e50
Jaspreet <jaspreetsingh44555@gmail.com>
2024-10-28 16:19:48 +0530

    Optimize lazy loading for images in AdvisorSection component

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -134,7 +134,7 @@ const BlogDetail = () => {
                   ></textarea>
                   <button
                     type="submit"
-                    className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+                    className="mt-4 px-6 py-2 bg-blue-950 text-white rounded-lg hover:bg-blue-900 transition"
                   >
                     Submit Feedback
                   </button>
